5d4068a893c507442f46ad4284750f15da0db7b6
hazem_m.o <hm3185@fayoum.edu.eg>
2024-06-13 23:55:25 +0300

    new

---
+++ src/pages/JopReady.tsx
@@ -0,0 +1,13 @@
+"use client";
+import PricingPage from '@/components/Pricing/PricingPage'
+import React from 'react'
+
+const JopReady: React.FC = () => {
+    return (
+        <div className='relative w-full pt-28'>
+            <PricingPage />
+        </div>
+    )
+}
+
+export default JopReady
