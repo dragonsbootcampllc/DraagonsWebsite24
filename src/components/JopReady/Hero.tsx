c19086c25860416232683b670d1e4d117dca7bbe
devbn3li <fudwhc@gmail.com>
2024-06-16 21:35:39 +0300

    Hero section updated

--- src/components/JopReady/Hero.tsx
+++ src/components/JopReady/Hero.tsx
@@ -15,7 +15,7 @@ const News = {
 };
 const Hero: React.FC = () => {
   return (
-    <div className='hero   min-h-[700px] md:min-h-[calc(100vh-12rem)] '>
+    <div className='hero relative  min-h-[700px] md:min-h-[calc(100vh-12rem)] '>
       <div className='absolute opacity-30  h-full w-full '>
         <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
       </div>
