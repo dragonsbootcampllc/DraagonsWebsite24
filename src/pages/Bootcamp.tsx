ba339b7cb52afe83a6d47a9b57613bf01a0fbec0
devbn3li <fudwhc@gmail.com>
2024-06-16 19:44:27 +0300

    infinite moving cards compo

--- src/pages/Bootcamp.tsx
+++ src/pages/Bootcamp.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { StickyScroll } from "../components/ui/sticky-scroll-reveal";
+import { InfiniteMovingCards } from "../components/ui/infinite-moving-cards";
 
 const BootcampPage = () => {
   const content = [
@@ -89,25 +90,50 @@ const BootcampPage = () => {
     },
   ];
 
+  const programe = [
+    {
+      title: "Students",
+      description:
+        "If you are a computer science graduate or someone with a strong IT background, Dragons Tech Bootcamp can help you sharpen your skills and prepare for a successful career in software development. Our intensive program is designed to bridge the gap between academic knowledge and industry requirements, ensuring you're ready to tackle real-world challenges.",
+    },
+    {
+      title: "Career Shifters",
+      description:
+        "Looking to make a complete switch to the software development track from another field? Our Bootcamp is perfect for those wanting to transition into the tech industry. We provide comprehensive training and support to help you acquire the necessary skills and confidence to thrive in a new, exciting career.",
+    },
+    {
+      title: "Upskilling Developers",
+      description: "Already a developer but looking to boost your skills? Our advanced courses are tailored to help you stay ahead in the rapidly evolving tech landscape. Learn the latest technologies, improve your coding efficiency, and enhance your problem-solving capabilities with our expert-led sessions.",
+    },
+    {
+      title: "Tech Entrepreneurs",
+      description:
+        "As a tech entrepreneur, understanding the intricacies of technology is crucial for your business success. Our Bootcamp provides you with a solid foundation in software development, enabling you to integrate tech solutions into your business strategy better and bridge the gap between tech and other fields you excel in.",
+    },
+  ];
 
   return (
     <div className="min-h-screen bg-[#030014] text-white flex flex-col items-center py-10 px-4 md:px-8">
       <div className="text-center flex flex-col gap-10 max-w-5xl mt-12 md:mt-28 w-full">
-        <div className="my-10 max-md:mt-20">
-          <h1 className="lg:text-6xl text-3xl font-bold mb-6">
-            Elevate Your Skills with Our Intensive{" "}
-            <span className="text-[#facc15]">6-Month</span> Tech Bootcamp
-          </h1>
-          <p className="text-lg flex px-2 text-center text-purple-300/50 justify-center ">
-            Master key technologies and leadership skills in our intensive
-            Bootcamp, designed for rapid, real-world success.
-          </p>
-        </div>
-        <Link
+        <div>
+          <div className="my-10 max-md:mt-20">
+            <h1 className="lg:text-6xl text-3xl font-bold mb-6">
+              Elevate Your Skills with Our Intensive{" "}
+              <span className="text-[#facc15]">6-Month</span> Tech Bootcamp
+            </h1>
+            <p className="text-lg flex px-2 text-center text-purple-300/50 justify-center ">
+              Master key technologies and leadership skills in our intensive
+              Bootcamp, designed for rapid, real-world success.
+            </p>
+          </div>
+          <div className="px-10">
+            <StickyScroll content={content} />
+          </div>
+          <Link
             href="https://docs.google.com/forms/d/e/1FAIpQLSf6yGyk40x0nNQwP_XSP_dFjiS4I6_7UoAzJDGRunl-NGRUsQ/viewform?usp=pp_url"
             target="_blank"
           >
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-8">
               <div className="group cursor-pointer select-none inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 pl-4 rounded-full shadow-md">
                 <p className="mr-2  inline-block text-white text-sm">
                   6 Month Bootcamp (Early access)
@@ -131,14 +157,27 @@ const BootcampPage = () => {
               </div>
             </div>
           </Link>
-
-        <div className="p-10">
-          <StickyScroll content={content} />
         </div>
 
-        <div className="flex flex-col gap-10 mt-8">
+        <div className="my-10">
+          <h1 className="lg:text-6xl text-3xl font-bold mb-6 mt-10">
+            Why You Should Take Dragons Tech Bootcamp
+          </h1>
+          <p className="text-lg flex px-2 text-center text-purple-300/50 justify-center ">
+            What Are Our Programs For?
+          </p>
+          <div className="h-[20rem] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+            <InfiniteMovingCards
+              items={programe}
+              direction="right"
+              speed="slow"
+            />
+          </div>
+        </div>
 
-          <div className="grid pt-40 mb-28 place-items-center   ">
+        <div className="flex flex-col gap-10">
+
+          <div className="grid my-20 place-items-center   ">
             <div
               style={{ fontFamily: "interV" }}
               className="text-6xl  content-center  max-w-4xl text-center md:text-7xl   z-10 flex justify-center m-auto relative  mb-3 text-gray-200  font-semibold	"
