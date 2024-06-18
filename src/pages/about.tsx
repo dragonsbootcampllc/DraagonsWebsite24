f03613bda0511de3f73e614f742f338e0d3fb100
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-18 04:36:51 +0300

    story update (Fajr)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,21 @@ import Hero from "@/components/JopReady/Hero";
 import Image from "next/image";
 import React from "react";
 
-export default function AboutUsPage(props: any) {
+interface Section {
+  mainHeader: string;
+  header: string;
+  paragraph: string;
+  img: string;
+}
+
+interface AboutUsProps {
+  mainHeader: string;
+  subHeader: string;
+  description: string;
+  story: Section[];
+}
+
+const AboutUsPage: React.FC<AboutUsProps> = (props) => {
   const HeroObj = {
     News: {
       id: 1,
@@ -10,98 +24,114 @@ export default function AboutUsPage(props: any) {
       content: "New Season just landed💯",
       link: "/blog/news",
     },
-    subHeadingText: " With Dragons Bootcamp in ",
-    heroHighlightedText: "Only 6 Month",
-    heroHeading: "Become Job Ready ",
-    Disctiption: "Join Dragons Bootcamp and become job-ready in just 6 months. Gain essential skills and hands-on experience for a successful career",
+    heroHeading: "The story of a ",
+    subHeadingText: "Student activity that ",
+    heroHighlightedText: "turned into a company",
+    Disctiption:
+      "Discover how a student activity became a thriving company. Join us and be part of our next chapter!",
   };
-  return (
-    <div id="aboutus" className="relative w-full">
-         <Hero
-              News={HeroObj.News}
-              subHeadingText={HeroObj.subHeadingText}
-              heroHighlightedText={HeroObj.heroHighlightedText}
-              heroHeading={HeroObj.heroHeading}
-              Disctiption={HeroObj.Disctiption}
-      />
-      <div className="flex justify-center flex-col items-center w-screen lg:mt-56 md:mt-28  mt-20">
-        <div className="flex sm:p-4 p-6 items-center justify-center flex-col gap-3 my-24 sm:w-4/5 md:w-4/5 lg:w-3/5">
-          <h1 className="md:text-2xl text-xl md:text-center text-start font-medium text-purple-800 uppercase">
-            {props.mainHeader}
-          </h1>
-          <h2 className="md:text-5xl text-3xl font-black md:text-center text-start py-3 md:px-10">
-            {props.subHeader}
-          </h2>
-          <p className="md:text-center text-start md:w-3/5 text-base font-normal">
-            {props.description}
-          </p>
-        </div>
 
-        <div className="flex sm:p-4 p-6 items-center justify-center flex-col gap-3 md:my-24 sm:w-4/5 md:w-4/5 lg:w-3/5">
-          <h1 className="md:text-xl text-lg text-start font-medium text-purple-800 uppercase">
-            {props.story.mainHeader}
-          </h1>
-          <div className="flex py-5 md:flex-row flex-col gap-3">
-            <div className="flex flex-col md:w-1/2 gap-3">
-              <h2 className="md:text-3xl text-2xl font-black text-start py-3">
-                {props.story.mainPart.header}
-              </h2>
-              <p className="text-start text-base font-normal">
-                {props.story.mainPart.paragraph}
-              </p>
-            </div>
-            <div className="flex flex-col justify-center items-center md:w-1/2">
-              <Image
-                src={props.story.mainPart.img}
-                alt="founders"
-                className="md:w-10/12 w-full rounded-lg"
-                width={100}
-                height={100}
-              />
-            </div>
+  return (
+    <div id='aboutus' className='relative w-full'>
+      <Hero
+        News={HeroObj.News}
+        subHeadingText={HeroObj.subHeadingText}
+        heroHighlightedText={HeroObj.heroHighlightedText}
+        heroHeading={HeroObj.heroHeading}
+        Disctiption={HeroObj.Disctiption}
+      />
+      <div className='flex justify-center flex-col items-center w-screen lg:mt-56 md:mt-28  mt-20'>
+        <div className='w-full  place-items-center grid gap-2 max-w-[1400px]'>
+          <div
+            style={{ fontFamily: "interV" }}
+            className='text-3xl min-h-screen flex-col content-center max-w-4xl text-center md:text-5xl z-10 flex justify-center m-auto relative mb-3 text-gray-200 font-semibold'>
+            <h1 className='md:text-xl  text-center text-lg  font-medium text-purple-800 uppercase'>
+              {props.mainHeader}
+            </h1>
+            <h1>
+              {props.subHeader}
+              <span className='bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text'></span>
+            </h1>
+            <p
+              style={{ fontFamily: "interV" }}
+              className='text-white/50  text-lg md:text-base head container mx-auto flex flex-col gap-7 justify-center text-center max-w-[800px] pt-5'>
+              {props.description}
+            </p>
           </div>
 
-          <div className="flex pt-16 md:flex-row flex-col-reverse gap-3">
-            <div className="flex flex-col justify-center items-center h-full md:w-2/3">
-              <Image
-                src={props.story.secondPart.img}
-                alt="founders"
-                className="md:w-11/12 w-full h-full rounded-lg"
-                width={100}
-                height={100}
-              />
+          <div className='my-5 min-h-screen md:my-12'>
+            <div className='w-full place-items-center grid  gap-2 max-w-[1400px]'>
+              <div
+                style={{ fontFamily: "interV" }}
+                className='text-3xl py-5 flex-col content-center max-w-4xl text-center md:text-5xl z-10 flex justify-center m-auto relative mb-3 text-gray-200 font-semibold'>
+                <h1 className='md:text-xl   text-center text-lg  font-medium text-purple-800 uppercase'>
+                  {props.story[0].mainHeader}
+                </h1>
+                <h1>
+                  {props.subHeader}
+                </h1>
+              </div>
             </div>
-            <div className="md:w-1/3 flex md:py-16 justify-center items-center">
-              <p className="text-start text-base font-normal">
-                {props.story.secondPart.paragraph}
-              </p>
-            </div>
+            {props.story.map((section: Section, index: number) => (
+              <div
+                key={index}
+                className={`flex max-w-[1200px] py-5 ${
+                  index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
+                } flex-col gap-3`}>
+                <div className='flex flex-col md:w-1/2 gap-3'>
+                  <h2 className='md:text-3xl text-2xl font-black text-start py-3'>
+                    {section.header}
+                  </h2>
+                  <p className='text-start text-base font-normal'>
+                    {section.paragraph}
+                  </p>
+                </div>
+                <div className='flex flex-col justify-center items-center md:w-1/2'>
+                  <Image
+                    src={section.img}
+                    alt='founders'
+                    className='md:w-10/12 w-full rounded-3xl'
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 AboutUsPage.defaultProps = {
   mainHeader: "OUR mission",
-  subHeader: "To bring real-life interactions to the virtual world seamlessly",
+  subHeader: "Empowering the Next Generation of Tech Innovators",
   description:
-    "The increasingly virtual, remote first world, needs an infrastructure provider that allows these experiences to happen seamlessly. 100ms' best-in-class quality and ease of use are fueling this revolution",
-
-  story: {
-    mainHeader: "OUR story",
-    mainPart: {
-      header: "Making video work",
+    "To empower aspiring tech professionals by providing immersive, hands-on training that transforms beginners into industry-ready experts. We are dedicated to fostering a supportive learning environment, bridging the skills gap, and driving innovation in the tech industry.",
+  story: [
+    {
+      mainHeader: "Our Story",
+      header: "Inception and Founding",
       paragraph:
-        "Kshitij started working on live video way back in 2000s when it wasn't really a thing. He built video solutions for the first Iphone & 3G networks, and then joined Facebook to build the video infrastructure for FB Live. After flying back to India, he joined as VP engineering at Disney+Hotstar, where he built the world's largest live streaming platform with his colleagues - Sarvesh and Aniket.",
+        "In Oct 2022, Dragons Bootcamp LLC was born out of a shared vision between two pioneers, Hamdy Saad and Hatem Ali. Hatem, a determined Computer Science student at EElu Fayoum branch, recognized a glaring disparity between traditional academic teachings and the practical skills needed in the tech industry. Despite the supportive academic environment, there was a noticeable gap in preparing students for real-world challenges.",
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2KdfZJkryOCFni4-CUywBU_aFnRntkbniw&s",
     },
-    secondPart: {
-      header: "Building 100ms",
+    {
+      mainHeader: "Our Story",
+      header: " The Journey Begins",
       paragraph:
-        "In 2020, they decided to build 100ms to solve the problem of building real-time video applications. They wanted to make it easy for developers to build video applications without worrying about the underlying infrastructure. They have built a team of passionate engineers and designers who are working hard to make this vision a reality.",
+        "Encouraged by the openness and support of Dr. Mohammed Agha, alongside teaching assistants Dr. Rabiee Ayman and Kareem Mangoud, Hatem embarked on a mission. He sought to empower his peers by providing practical, industry-focused insights that were often missing from the standard curriculum. His initiative quickly gained momentum, driven by a passion to bridge the divide between theory and application.",
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2KdfZJkryOCFni4-CUywBU_aFnRntkbniw&s",
     },
-  },
+    {
+      mainHeader: "Our Story",
+      header: " Dragons Bootcamp's Inaugural Webinar at EELU",
+      paragraph:
+        "Partnering with Hatem, Hamdy Saad, an accomplished entrepreneur working remotely for a Dubai-based company at this time, brought his expertise from founding tech ventures such as Divzoon and Dlink. Their collaboration culminated in Dragons Bootcamp's inaugural webinar at EELU, where guest speaker Dario Lencina was featured. This event not only showcased Dragons Bootcamp's innovative approach but also sparked enthusiasm among students and educators.",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2KdfZJkryOCFni4-CUywBU_aFnRntkbniw&s",
+    },
+  ],
 };
+
+export default AboutUsPage;
