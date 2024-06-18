210a379930e6a8a9b51ad3c35a82e8736b023143
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-18 05:26:58 +0300

    responsive Hero

--- src/components/Micros/ButtonLite.tsx
+++ src/components/Micros/ButtonLite.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Button } from "@nextui-org/react";
 
 interface ButtonComponentProps {
   CTAtext: string;
 }
 
-function ButtonComponent({ CTAtext }: ButtonComponentProps) {
+const ButtonComponent: React.FC<ButtonComponentProps> = ({ CTAtext }) => {
   return (
-    <button className='relative group cursor-pointer  shadow-2xl rounded-full p-px text-white font-semibold leading-6 inline-block w-full h-full'>
+    <button className='relative group cursor-pointer shadow-2xl rounded-full p-px text-white font-semibold leading-6 inline-block w-full h-full'>
       <span className='absolute inset-0 overflow-hidden rounded-full'>
-        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100  rotate-180' />
+        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100 rotate-180' />
       </span>
       <div className='relative flex items-center justify-center h-full w-full z-10 rounded-full py-2 px-4 ring-1 ring-white/10 text-base sm:text-lg md:text-xl'>
         <span>{CTAtext}</span>
@@ -20,6 +19,3 @@ function ButtonComponent({ CTAtext }: ButtonComponentProps) {
 }
 
 export default ButtonComponent;
-
-
-      
