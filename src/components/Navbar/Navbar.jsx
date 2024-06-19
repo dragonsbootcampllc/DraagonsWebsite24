3270c6c2e23753ff9594c2633876efd421103711
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-19 05:55:52 +0300

    npm fix (production ready)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { React, useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -15,14 +15,14 @@ const Dropdown_V2 = dynamic(() => import("./Dropdown_V.2"), {
   ssr: false,
 });
 
-import levelUp_O2_img from '@/../public/assets/Dash/ESGNAV/o2.png';
+import levelUp_O2_img from "@/../public/assets/Dash/ESGNAV/o2.png";
 import LogoImage from "@/../public/Identity/Images/2023/SVG/test.svg";
-import sustracker_img from '@/../public/assets/Dash/ESGNAV/sustracker.png';
-import datalab_img from '@/../public/assets/Dash/SolutionNAV/lab.png';
-import pulse_img from '@/../public/assets/Premasset/279b4dee-988e-4168-9745-09c71397741a.png';
-import resources_img from '@/../public/assets/Dash/academy/resources.png';
-import workshop_img from '@/../public/assets/Dash/academy/workshop.png';
-import Student_img from '@/../public/assets/calculate.png';
+import sustracker_img from "@/../public/assets/Dash/ESGNAV/sustracker.png";
+import datalab_img from "@/../public/assets/Dash/SolutionNAV/lab.png";
+import pulse_img from "@/../public/assets/Premasset/279b4dee-988e-4168-9745-09c71397741a.png";
+import resources_img from "@/../public/assets/Dash/academy/resources.png";
+import workshop_img from "@/../public/assets/Dash/academy/workshop.png";
+import Student_img from "@/../public/assets/calculate.png";
 import {
   IoLeaf,
   IoStatsChart,
@@ -36,8 +36,8 @@ import {
   IoWarning,
   IoSearchSharp,
   IoShieldCheckmarkOutline,
-  IoWarningOutline
-} from 'react-icons/io5';
+  IoWarningOutline,
+} from "react-icons/io5";
 
 import {
   FaChalkboardTeacher,
@@ -45,12 +45,11 @@ import {
   FaBroadcastTower,
   FaRobot,
 } from "react-icons/fa";
-import business_img from '@/../public/assets/Dash/Solutions/Biz.png';
-import ESG_img from '@/../public/assets/Dash/Solutions/ESG.png';
+import business_img from "@/../public/assets/Dash/Solutions/Biz.png";
+import ESG_img from "@/../public/assets/Dash/Solutions/ESG.png";
 import ButtonComponent from "../Micros/Button";
 import ButtonLite from "@/components/Micros/ButtonLite";
 import Button from "@/components/Micros/Button";
-
 
 const simpleFloatAnimation = {
   y: ["-20px", "22px"],
@@ -69,37 +68,45 @@ const SolutionTabData = {
     path: "http://dragons.divzoon.com/solutions/students/",
     description:
       "Advance your career with Dragoons Bootcamp’s targeted courses in data analysis and full-stack development.",
-    Icon: <motion.img src={Student_img} alt="icon" animate={simpleFloatAnimation} />,
+    Icon: (
+      <motion.img src={Student_img} alt='icon' animate={simpleFloatAnimation} />
+    ),
   },
   tab2: {
     title: "Partners",
     path: "http://dragons.divzoon.com/solutions/partners/",
     description:
       "Enhance your team with our ready-to-deploy graduates skilled in the latest tech trends.",
-    Icon: <motion.img src={Student_img} alt="icon" animate={simpleFloatAnimation} />,
+    Icon: (
+      <motion.img src={Student_img} alt='icon' animate={simpleFloatAnimation} />
+    ),
   },
   tab1BottomLinks: [
     {
       title: "Become JopReady",
-      description: "Get ready for the job market with our career-focused bootcamps.",
+      description:
+        "Get ready for the job market with our career-focused bootcamps.",
       path: "/JopReady",
       Icon: IoStatsChart,
     },
     {
       title: "Mentorship",
-      description: "Get guidance from industry experts and level up your skills.",
+      description:
+        "Get guidance from industry experts and level up your skills.",
       path: "/Bootcamp",
       Icon: IoBusiness,
     },
     {
       title: "Thought Leader",
-      description: "Learn from the best in the industry and stay ahead of the curve.",
+      description:
+        "Learn from the best in the industry and stay ahead of the curve.",
       path: "/Bootcamp",
       Icon: IoBusiness,
     },
     {
       title: "Students activity",
-      description: "Get involved in our student community and grow your network.",
+      description:
+        "Get involved in our student community and grow your network.",
       path: "/DSA",
       Icon: IoTrendingUp,
     },
@@ -119,18 +126,20 @@ const SolutionTabData = {
     },
     {
       title: "Software Outsourcing",
-      description: "Outsource your software development to our top graduates and save time and money.",
+      description:
+        "Outsource your software development to our top graduates and save time and money.",
       path: "/Webinar",
       Icon: IoShieldCheckmarkOutline,
     },
     {
       title: " Intern Program Managmenet",
-      description: "Manage your intern program with our HRMS and get access to top tech talent.",
+      description:
+        "Manage your intern program with our HRMS and get access to top tech talent.",
       path: "/DSA",
       Icon: IoPeople,
-    }
+    },
   ],
-}
+};
 
 const TallentsTabData = {
   title: "Tallents",
@@ -139,21 +148,24 @@ const TallentsTabData = {
     path: "/Talents",
     description:
       "Our top 3% graduates excel in tech, gaining access to exclusive job opportunities and leading the way in industry success.",
-    Icon: <motion.img src={ESG_img} alt="icon" animate={simpleFloatAnimation} />,
+    Icon: (
+      <motion.img src={ESG_img} alt='icon' animate={simpleFloatAnimation} />
+    ),
   },
   tab2: {
     title: "Partners",
     path: "service/business",
     description:
       "Spot and recruit top tech talent seamlessly through our HRMS, enhancing your team with industry-leading expertise",
-    Icon: <motion.img src={business_img} alt="icon" animate={simpleFloatAnimation} />,
+    Icon: (
+      <motion.img
+        src={business_img}
+        alt='icon'
+        animate={simpleFloatAnimation}
+      />
+    ),
   },
-
 };
-
-
-
-
 
 const Navbar = () => {
   // State hooks for controlling UI elements
@@ -190,7 +202,6 @@ const Navbar = () => {
     closeNavigation();
   };
 
-
   // Function to bring the user to the top of the page
   const scrollToTop = () => {
     window.scrollTo({
@@ -200,64 +211,64 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-purple-900 px-8 backdrop-blur-3xl bg-opacity-50  border-b border-white/20 py-4 fixed top-0 font-inter m-auto -translate-x-1/2 z-50 left-1/2 flex justify-center w-full  shadow-md">
-      <div className="flex gap-3 justify-between items-center  max-[1496px]:px-0 max-w-[1400px] box-border w-full  h-[4.5rem]">
-        <div className="z-50 max-[1496px]:w-full max-[1496px]:flex max-[1496px]:p-[5%] max-[1496px]:h-full justify-between items-center">
+    <nav className='bg-purple-950/20 px-8 backdrop-blur-3xl bg-opacity-50  border-b border-purple-900/20 py-4 fixed top-0 font-inter m-auto -translate-x-1/2 z-50 left-1/2 flex justify-center w-full  shadow-md'>
+      <div className='flex gap-3 justify-between items-center  max-[1496px]:px-0 max-w-[1400px] box-border w-full  h-[2.5rem]'>
+        <div className='z-50 max-[1496px]:w-full max-[1496px]:flex max-[1496px]:p-[5%] max-[1496px]:h-full justify-between items-center'>
           {/* Logo */}
-          <div className="left flex gap-3 justify-center items-center">
-            <Link href="/" className="flex gap-[6px] items-center">
-              <div className="w-32 md:w-48">
+          <div className='left flex gap-3 justify-center items-center'>
+            <Link href='/' className='flex gap-[6px] items-center'>
+              <div className='w-32 md:w-48'>
                 <img
-                  className="aspect-square"
+                  className='aspect-square'
                   src={LogoImage.src}
-                  alt="Dragons Bootcamp"
+                  alt='Dragons Bootcamp'
                 />
               </div>
             </Link>
             <div
               className={
                 "border px-2 py-1 text-xs max-[370px]:hidden select-none hover:bg-fuchsia-800 duration-300 cursor-pointer  justify-center text-center grid place-items-center m-auto h-6 w-12 relative rounded-full"
-              }
-            >
-              <span className={"absolute"}>DEMO</span>
+              }>
+              <span className={"absolute animate-pulse duration-300"}>
+                DEMO
+              </span>
             </div>
           </div>
 
           {/* Logo and Hamburger Menu */}
           <button
-            className="flex min-[1497px]:hidden flex-col aspect-square w-12 justify-center items-center gap-[6px]"
-            onClick={toggleNavigation}
-          >
+            className='flex min-[1497px]:hidden flex-col aspect-square w-12 justify-center items-center gap-[6px]'
+            onClick={toggleNavigation}>
             {/* Simplified class names for readability */}
             <span
-              className={`w-6 h-[2px] bg-white transition-all duration-[.45s] ${isNavOpen
-                ? "-rotate-45 translate-y-2 delay-300"
-                : "rotate-0 translate-y-0"
-                }`}
-            ></span>
+              className={`w-6 h-[2px] bg-white transition-all duration-[.45s] ${
+                isNavOpen
+                  ? "-rotate-45 translate-y-2 delay-300"
+                  : "rotate-0 translate-y-0"
+              }`}></span>
             <span
-              className={`h-[2px] bg-white transition-all duration-[.45s] ${isNavOpen ? "w-0" : "w-6 delay-300"
-                }`}
-            ></span>
+              className={`h-[2px] bg-white transition-all duration-[.45s] ${
+                isNavOpen ? "w-0" : "w-6 delay-300"
+              }`}></span>
             <span
-              className={`w-6 h-[2px] bg-white transition-all duration-[.45s] ${isNavOpen
-                ? "rotate-45 -translate-y-2 delay-300"
-                : "rotate-0 translate-y-0"
-                }`}
-            ></span>
+              className={`w-6 h-[2px] bg-white transition-all duration-[.45s] ${
+                isNavOpen
+                  ? "rotate-45 -translate-y-2 delay-300"
+                  : "rotate-0 translate-y-0"
+              }`}></span>
           </button>
         </div>
 
         {/* Navigation Links and CTA Button */}
         <div
-          className={`flex max-[1496px]:bg-[#2c1a3a] items-center min-[1496px]:justify-end max-[1496px]:flex-col text-[20px] font-normal h-full max-[1496px]:w-full gap-3 max-[1496px]:gap-0 max-[1496px]:overflow-y-scroll max-[1496px]:overflow-x-hidden w-screen  flex-grow text-center transition-all box-border duration-[.45s] ${isNavOpen
-            ? "max-[1496px]:translate-y-0"
-            : "max-[1496px]:-translate-y-[120%]"
-            } top-full left-0 text-black  max-[1496px]:flex-col max-[1496px]:absolute max-[1496px]:w-full max-[1496px]:pb-[6.5rem] max-[1496px]:h-[calc(100vh-4.5rem)] max-[1496px]:text-lg max-[1496px]:border-b max-[1496px]:border-black`}
-        >
+          className={`flex max-[1496px]:bg-[#2c1a3a] items-center min-[1496px]:justify-end max-[1496px]:flex-col text-lg font-normal h-full max-[1496px]:w-full gap-3 max-[1496px]:gap-0 max-[1496px]:overflow-y-scroll max-[1496px]:overflow-x-hidden w-screen  flex-grow text-center transition-all box-border duration-[.45s] ${
+            isNavOpen
+              ? "max-[1496px]:translate-y-0"
+              : "max-[1496px]:-translate-y-[120%]"
+          } top-full left-0 text-black  max-[1496px]:flex-col max-[1496px]:absolute max-[1496px]:w-full max-[1496px]:pb-[6.5rem] max-[1496px]:h-[calc(100vh-4.5rem)] max-[1496px]:text-lg max-[1496px]:border-b max-[1496px]:border-black`}>
           <NavbarLink
-            path="/useCases"
-            title="Use Cases"
+            path='/useCases'
+            title='Use Cases'
             toggleNavigation={toggleNavigation}
             closeNavigation={closeNavigation}
             isActive={activeLink === "/useCases"} // Pass active state
@@ -283,37 +294,37 @@ const Navbar = () => {
           {/* Static Navigation Links */}
 
           <NavbarLink
-            path="/events"
-            title="Events"
+            path='/events'
+            title='Events'
             toggleNavigation={toggleNavigation}
             closeNavigation={closeNavigation}
             isActive={activeLink === "/events"} // Pass active state
             onClick={() => setActiveLink("/events")} // Update active state
           />
           <NavbarLink
-            path="/about"
-            title="About Us"
+            path='/about'
+            title='About Us'
             toggleNavigation={toggleNavigation}
             closeNavigation={closeNavigation}
             isActive={activeLink === "/about"} // Pass active state
             onClick={() => setActiveLink("/about")} // Update active state
           />
           <NavbarLink
-            path="/Pricing "
-            title="Pricing "
+            path='/Pricing '
+            title='Pricing '
             toggleNavigation={toggleNavigation}
             closeNavigation={closeNavigation}
             isActive={activeLink === "/Pricing"} // Pass active state
             onClick={() => setActiveLink("/Pricing")} // Update active state
           />
           {/* CTA Button */}
-          <div className='h-16 max-w-[350px] flex max-[402px]:flex-col px-5 w-full justify-between gap-3'>
-            <ButtonLite
-              CTAtext="Boot a Demo"
-            />
-            <ButtonComponent
-              CTAtext="Apply Now"
-            />
+          <div className='h-16 max-w-[350px] relative  grid place-items-center md:grid-cols-2   w-full  gap-3'>
+            <div className='h-12 hidden md:block w-full'>
+            </div>
+
+            <div className='h-12 w-full'>
+              <ButtonComponent className='!text-sm' CTAtext='Apply Now' />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Navbar/SubNavbarLink_V.3.jsx
+++ src/components/Navbar/SubNavbarLink_V.3.jsx
@@ -8,7 +8,7 @@ const SubNavbarLink_V3 = ({ Icon, title, path, clear }) => {
   return (
     <Link href={path} className={`active:scale-90 group flex justify-between items-center gap-2 rounded-lg font-semibold p-3 transition-all group/subLink bg-[#8e44ad]/50 hover:bg-[#8e44ad] w-full`} onClick={clear}>
       <span className="text-lg group-hover/subLink:animate-pulse text-white">{title}</span>
-      <span className={`rounded-lg text-xl transition-all group-hover/subLink:-rotate-12  group-hover/subLink:scale-105 duration-500 !text-white `}>
+      <span className={`rounded-lg text-lg transition-all group-hover/subLink:-rotate-12  group-hover/subLink:scale-105 duration-500 !text-white `}>
         <Icon />
       </span>
     </Link>
