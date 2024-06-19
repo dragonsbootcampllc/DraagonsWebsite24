bd1fbbd62bc989242d29294fd0634cb0ca52f82e
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-19 05:55:52 +0300

    npm fix (production ready)

--- src/components/Navbar/NavbarLink.jsx
+++ src/components/Navbar/NavbarLink.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 // NavbarLink component. Each link element in the navbar
 const NavbarLink = ({ path, title, closeNavigation, }) => {
   // Tailwind CSS classes for the Link component
-  const linkClasses = 'px-4 min-[1130px]:h-full max-[1130px]:py-8 text-white/70 hover:text-white font-semibold after:bg-white  transition-all max-[1130px]:w-full flex items-center justify-center duration-[.45s] max-[991px]:hover:bg-transparent max-[991px]:px-0 relative after:absolute after:h-[2px] after:bg-[#121212] after:w-0 after:left-1/2 after:-translate-x-1/2 after:hover:w-full after:transition-all after:bottom-0';
+  const linkClasses = 'px-4 min-[1130px]:h-full max-[1130px]:py-8 text-white/70 text-lg hover:text-white font-semibold after:bg-white  transition-all max-[1130px]:w-full flex items-center justify-center duration-[.45s] max-[991px]:hover:bg-transparent max-[991px]:px-0 relative after:absolute after:h-[2px] after:bg-[#121212] after:w-0 after:left-1/2 after:-translate-x-1/2 after:hover:w-full after:transition-all after:bottom-0';
   
   return (
     <Link href={path} className={linkClasses} onClick={closeNavigation}>
