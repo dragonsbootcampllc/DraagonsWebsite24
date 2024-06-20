9045a8f5b97b5664ec45a1ad441b8d581541123c
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-20 07:15:03 +0300

    Student activity

--- src/components/Cards/TrainingServiceSection-component/TrainingServiceSection.jsx
+++ src/components/Cards/TrainingServiceSection-component/TrainingServiceSection.jsx
@@ -1,50 +1,5 @@
-import { image } from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
-import Bootcamp6Month from "@/../public/assets/6MonthBootcamp.png";
-import DragonsSprints from "@/../public/assets/DragonsSprints.png";
-import fundamentals from "@/../public/assets/fundamentals.png";
-
-const HeadData = {
-  badge: "Training Service Section",
-  title_1: "Empower Your ",
-  title_span: "Tech Career",
-  title_2: " Career with Us!",
-  description:
-    "Select a program that aligns with your goals. Whether you're a Beginner or looking to join our intensive six-month Bootcamp, we offer a variety of options tailored to your needs.",
-};
-
-const CardsData = [
-  {
-    label: "Comming Soon",
-    isDisabled: true,
-    title: "Fundemental track",
-    description:
-      "be a real software engineer and Master problem-solving, data structures, algorithms, and more to become a proficient software engineer.",
-    overlayStyle:
-      "radial-gradient(136.22% 75.8% at 50% 27.57%, #171619 0%, rgba(23, 22, 25, 1) 61.62%, rgba(0, 49, 239, 1) 79.17%, rgba(251, 53, 208, 1) 100%)",
-    image: fundamentals.src,
-  },
-  {
-    label: "Special",
-    isPremier: true,
-    title: "Dragon's Bootcamp",
-    description:
-      " Our six-month, full-time program is designed to help you master the skills needed to launch a career in software engineering.",
-    overlayStyle:
-      "radial-gradient(329.95% 173.05% at 50.04% 100%, #fc36d0 0%, rgba(252, 54, 208, 1) 0.52%, rgba(0, 49, 239, 1) 18.71%, rgba(23, 22, 25, 1) 28.65%, rgba(23, 22, 25, 1) 100%)",
-    image: Bootcamp6Month.src,
-  },
-  {
-    label: "Demo Available",
-    title: "Dragon's Sprints",
-    description:
-      "Short-term, intensive programs designed to help you master a specific skill or technology.",
-    overlayStyle:
-      "radial-gradient(136.22% 75.8% at 50% 27.57%, #171619 0%, rgba(23, 22, 25, 1) 61.62%, rgba(0, 49, 239, 1) 79.17%, rgba(251, 53, 208, 1) 100%)",
-    image: DragonsSprints.src,
-  },
-];
 
 const Badge = ({ text }) => (
   <div className='badge w-fit mx-auto text-white/70 rounded-lg border border-white/40 px-2 py-1'>
@@ -105,7 +60,7 @@ const Card = ({
   </Link>
 );
 
-function TrainingServiceSection() {
+function TrainingServiceSection({ HeadData, CardsData }) {
   return (
     <div className='w-full min-h-screen max-w-[1400px] flex flex-col gap-8 text-white'>
       <div className='head container mx-auto flex flex-col gap-7 justify-center text-center max-w-[800px] pt-5'>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,13 @@ import styles from "../styles/Home.module.css";
 import React, { ReactElement, ReactNode, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Bootcamp6Month from "@/../public/assets/6MonthBootcamp.png";
+import DragonsSprints from "@/../public/assets/DragonsSprints.png";
+import fundamentals from "@/../public/assets/fundamentals.png";
+import TechFocused from "@/../public/images/DSA/Tech.png";
+import Gamification from "@/../public/images/DSA/Gamification.png";
+import Collage from "@/../public/images/DSA/Collage.png";
+
 const Hero = dynamic(() => import("../components/Hero/Hero"), { ssr: false });
 const TechStack = dynamic(() => import("../components/TechStack/TechStack"), {
   ssr: false,
@@ -20,13 +27,83 @@ const GridSection = dynamic(
   () => import("../components/GridSection/GridSection"),
   { ssr: false }
 );
-const TrainingServiceSection = dynamic(
+import TrainingServiceSection from "@/components/Cards/TrainingServiceSection-component/TrainingServiceSection";
+const StudentActivitySection = dynamic(
   () =>
     import(
       "../components/Cards/TrainingServiceSection-component/TrainingServiceSection"
     ),
   { ssr: false }
 );
+const HeadData = {
+  badge: "Training Service Section",
+  title_1: "Empower Your ",
+  title_span: "Tech Career",
+  title_2: " Career with Us!",
+  description:
+    "Select a program that aligns with your goals. Whether you're a Beginner or looking to join our intensive six-month Bootcamp, we offer a variety of options tailored to your needs.",
+};
+
+const CardsData = [
+  {
+    label: "Comming Soon",
+    isDisabled: true,
+    title: "Fundamental track",
+    description:
+      "Be a real software engineer and master problem-solving, data structures, algorithms, and more to become a proficient software engineer.",
+    overlayStyle:
+      "radial-gradient(136.22% 75.8% at 50% 27.57%, #171619 0%, rgba(23, 22, 25, 1) 61.62%, rgba(0, 49, 239, 1) 79.17%, rgba(251, 53, 208, 1) 100%)",
+    image: fundamentals,
+  },
+  {
+    label: "Special",
+    isPremier: true,
+    title: "Dragon's Bootcamp",
+    description:
+      " Our six-month, full-time program is designed to help you master the skills needed to launch a career in software engineering.",
+    overlayStyle:
+      "radial-gradient(329.95% 173.05% at 50.04% 100%, #fc36d0 0%, rgba(252, 54, 208, 1) 0.52%, rgba(0, 49, 239, 1) 18.71%, rgba(23, 22, 25, 1) 28.65%, rgba(23, 22, 25, 1) 100%)",
+    image: Bootcamp6Month,
+  },
+  {
+    label: "Demo Available",
+    title: "Dragon's Sprints",
+    description:
+      "Short-term, intensive programs designed to help you master a specific skill or technology.",
+    overlayStyle:
+      "radial-gradient(136.22% 75.8% at 50% 27.57%, #171619 0%, rgba(23, 22, 25, 1) 61.62%, rgba(0, 49, 239, 1) 79.17%, rgba(251, 53, 208, 1) 100%)",
+    image: DragonsSprints,
+  },
+];
+
+const HeadDataStudentActivity = {
+  badge: "Student Activity Section",
+  title_1: "Tech-Driven  ",
+  title_span: " Student ",
+  title_2: "Initiative",
+  description:"Our students are constantly working on projects, attending workshops, and participating in hackathons. Check out some of their latest activities.",
+};
+
+const CardsDataStudentActivity = [
+  {
+    title: "Tech-Focused Activities",
+    description:
+      "DSA drives tech prowess through hands-on activities, enhancing student skills in collaboration with colleges",
+    image: TechFocused.src,
+  },
+  {
+    title: "Gamification and Digital Profiling",
+    description:
+      "Using gamification, DSA records profiles and activities, ensuring comprehensive documentation and quality assurance.",
+    image: Gamification.src,
+  },
+  {
+    title: "Quality Control Collaboration",
+    description:
+      "DSA partners with colleges for quality control, ensuring activities align with educational standards and industry needs.",
+    image: Collage.src,
+  },
+];
 
 // Dynamic imports
 const Sec2Summary = dynamic(() => import("../components/WhatWeDo"), {
@@ -47,7 +124,10 @@ export default function Home() {
       </div>
 
       <div className='w-full flex justify-center'>
-        <TrainingServiceSection />
+        <TrainingServiceSection
+          HeadData={HeadData}
+          CardsData={CardsData}
+        />
       </div>
 
       <div className='mt-12 w-full flex justify-center items-center'>
@@ -64,17 +144,21 @@ export default function Home() {
 
       <div className='relative flex justify-center flex-col'>
         <div className='w-auto relative'>
-        <TechStack
-  MainText={[
-    "Explore our robust ",
-    <span key='coloredText' className='header text-purple-800'>
-      Tech Stack
-    </span>,
-    " with latest technologies.",
-  ]}
-/>
-
+          <TechStack
+            MainText={[
+              "Explore our robust ",
+              <span key='coloredText' className='header text-purple-800'>
+                Tech Stack
+              </span>,
+              " with latest technologies.",
+            ]}
+          />
         </div>
+      </div>
+      <div className='w-full flex justify-center'>
+        <StudentActivitySection
+         HeadData={HeadDataStudentActivity} CardsData={CardsDataStudentActivity}
+        />
       </div>
 
       {/* <div className='my-28 w-full flex justify-center items-center'>
@@ -94,8 +178,6 @@ export default function Home() {
           </Link>
         </div>
       </div>
-
-     
 
       {popup && popup}
     </div>
