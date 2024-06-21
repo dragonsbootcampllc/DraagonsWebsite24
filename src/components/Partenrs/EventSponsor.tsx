b59a5c4a93c2ddc98cee2eb60987d3b305b8ad05
hazem_m.o <hm3185@fayoum.edu.eg>
2024-06-22 00:10:17 +0300

    Finsh Page

--- src/components/Partenrs/EventSponsor.tsx
+++ src/components/Partenrs/EventSponsor.tsx
@@ -4,61 +4,73 @@ import Link from "next/link";
 import Button from "@/components/Micros/Button";
 import ButtonLite from "@/components/Micros/ButtonLite";
 
+interface SponsorDataItem {
+    title: string;
+    description: string;
+    image: string;
+}
 
 const CTA = {
     ctaApplyText: "Apply Now",
     ctaReadMoreText: "Read More",
-}
+};
 
-const eventsSponsorData = [
+const eventsSponsorData: SponsorDataItem[] = [
     {
         title: "Tailored Brand Integration",
         description: "Customizing sponsorship packages to seamlessly integrate your brand into event activities and promotions.",
         image: "/images/supportive_community.jpg",
-
     },
     {
         title: "Brand Visibility",
         description: "Increasing brand exposure through event branding opportunities like logos on banners, promotional items, and digital platforms.",
         image: "/images/learning_companies.jpg",
-
     },
     {
         title: "Audience Engagement",
         description: "Facilitating interactive activities or workshops to engage attendees and strengthen your brand's connection with the audience.",
         image: "/images/industry_connections.jpg",
-
     },
 ];
+
 const headText = {
-    headlineText: "Empower",
-    heroHighlightedText: "  Your Events ",
+    headlineText: "Empower ",
+    heroHighlightedText: "Your Events",
     subheadlineText: " Through Strategic Sponsorship",
     descriptionText: "We provide sponsorship for various events to help you reach your audience and make your events successful.",
 };
+
 const EventSponsor: React.FC = () => {
     return (
-        <div className="w-full container mx-auto  flex flex-col  min-h-screen justify-center items-center gap-8 text-white">
-            <div className='heading md:max-w-[1000px] text-center'>
+        <div className="w-full container mx-auto flex flex-col min-h-screen justify-center items-center gap-8 text-white px-4 sm:px-6 lg:px-8">
+            <div className="heading md:max-w-[1000px] text-center">
                 <h1
                     style={{ fontFamily: "interV" }}
-                    className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-4xl mx-auto z-10 relative mb-3 text-gray-200 font-semibold'>
+                    className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-4xl mx-auto z-10 relative mb-3 text-gray-200 font-semibold"
+                >
                     {headText.headlineText}
-                    <span className='bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text'>
+                    <span className="bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text">
                         {headText.heroHighlightedText}
                     </span>
                     {headText.subheadlineText}
                 </h1>
-                <p className='text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8'>
+                <p className="text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8">
                     {headText.descriptionText}
                 </p>
             </div>
-            <div className="container  max-w-[1100px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div className="container max-w-[1100px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {eventsSponsorData.map((item, index) => (
-                    <div key={index} className=" rounded-2xl bg-purple-950 min-h-[400px] hover:shadow-[0_0_.5rem_0rem_rgba(251,53,208,1)]   relative overflow-hidden text-center pt-5 flex flex-col gap-5 cursor-pointer transition-all hover:scale-[1.02] duration-300">
-                        <h3 className='font-semibold text-3xl bg-gradient-to-r from-white to-gray-200  text-transparent bg-clip-text'>{item.title}</h3>
-                        <p className='text-[14px] text-center text-white/70 font-semibold '>{item.description}</p>
-                        <div className="w-full h-full relative">
+                    <div
+                        key={index}
+                        className="rounded-2xl bg-purple-950 min-h-[400px] hover:shadow-[0_0_.5rem_0rem_rgba(251,53,208,1)] relative overflow-hidden text-center pt-5 flex flex-col gap-5 cursor-pointer transition-all hover:scale-[1.02] duration-300"
+                    >
+                        <h3 className="font-semibold text-3xl bg-gradient-to-r from-white to-gray-200 text-transparent bg-clip-text">
+                            {item.title}
+                        </h3>
+                        <p className="text-[14px] sm:text-sm md:text-base lg:text-lg text-center text-white/70 px-3 font-semibold">
+                            {item.description}
+                        </p>
+                        <div className="w-full h-60 sm:h-80 md:h-full relative">
                             <Image
                                 src={item.image}
                                 alt={item.title}
@@ -71,21 +83,20 @@ const EventSponsor: React.FC = () => {
                     </div>
                 ))}
             </div>
-            <div className='text-center mt-8 flex flex-col sm:flex-row gap-4 justify-center'>
-                <Link href='/Pricing' passHref>
-                    <div className='h-12 w-48 md:h-16 md:w-64'>
+            <div className="text-center mt-8 flex flex-col sm:flex-row gap-4 justify-center">
+                <Link href="/Pricing" passHref>
+                    <div className="h-12 w-48 md:h-16 md:w-64">
                         <Button CTAtext={CTA.ctaApplyText} />
                     </div>
-                </Link >
-                <Link href='/Events' passHref>
-                    <div className='h-12 w-48 md:h-16 md:w-64'>
+                </Link>
+                <Link href="/Events" passHref>
+                    <div className="h-12 w-48 md:h-16 md:w-64">
                         <ButtonLite CTAtext={CTA.ctaReadMoreText} />
                     </div>
                 </Link>
             </div>
-
         </div>
     );
-}
+};
 
 export default EventSponsor;
--- src/components/Partenrs/GetTalents.tsx
+++ src/components/Partenrs/GetTalents.tsx
@@ -1,9 +1,16 @@
-"use client";
-import Link from "next/link";
+import React from "react";
 import Image from "next/image";
-import { GlareCard } from "../ui/glare-card";
+import Link from "next/link";
 import Button from "@/components/Micros/Button";
 import ButtonLite from "@/components/Micros/ButtonLite";
+import { GlareCard } from "../ui/glare-card";
+
+interface GlareCardData {
+    imageSrc: string;
+    title: string;
+    description: string;
+    description_Highlighted: string;
+}
 
 const content: {
     headlineText: string;
@@ -12,12 +19,7 @@ const content: {
     descriptionText: string;
     ctaApplyText: string;
     ctaReadMoreText: string;
-    glareCards: {
-        imageSrc: string;
-        title: string;
-        description: string;
-        description_Highlighted: string;
-    }[];
+    glareCards: GlareCardData[];
 } = {
     headlineText: "Discover",
     heroHighlightedText: " Outstanding Talent",
@@ -59,43 +61,42 @@ const GetTalents: React.FC = () => {
     } = content;
 
     return (
-        <div className='py-6 relative w-full rounded-t-3xl my-10 min-h-[700px] md:min-h-[calc(100vh-0rem)] grid place-items-center' id="CTAMaterial">
-            <div className='container mx-auto flex flex-col justify-center items-center gap-9 px-4 md:px-6 lg:px-8'>
-                <div className='heading md:max-w-[1000px] text-center'>
+        <div className=" relative w-full rounded-t-3xl my-10 min-h-[700px] md:min-h-[calc(100vh-0rem)] grid place-items-center" id="CTAMaterial">
+            <div className="container mx-auto flex flex-col justify-center items-center gap-9 px-4 md:px-6 lg:px-8">
+                <div className="heading md:max-w-[1000px] text-center">
                     <h1
                         style={{ fontFamily: "interV" }}
-                        className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-4xl mx-auto z-10 relative mb-3 text-gray-200 font-semibold'>
+                        className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-4xl mx-auto z-10 relative mb-3 text-gray-200 font-semibold"
+                    >
                         {headlineText}
-                        <span className='bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text'>
+                        <span className="bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text">
                             {heroHighlightedText}
                         </span>
                         {subheadlineText}
                     </h1>
-                    <p className='text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8'>
+                    <p className="text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8">
                         {descriptionText}
                     </p>
                 </div>
 
-                <div className='grid grid-cols-1  md:grid-cols-3 gap-6 md:gap-10'>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-10">
                     {glareCards.map((card, index) => (
-                        <GlareCard
-                            key={index}
-                            className='relative flex flex-col items-start justify-end py-8 px-6'>
+                        <GlareCard key={index} className="relative flex flex-col items-start justify-end py-8 px-6">
                             <Image
                                 src={card.imageSrc}
-                                className='h-full w-full absolute inset-0 object-cover rounded-3xl'
+                                className="h-full w-full absolute inset-0 object-cover rounded-3xl"
                                 alt={`Image of ${card.title}`}
                                 width={3388}
                                 height={2259}
                             />
                             {card.title && card.description && (
-                                <div className='absolute left-0 right-0 grid place-items-center p-4 text-center h-[30%] bg-purple-900/80 backdrop-blur-3xl w-full bottom-0 rounded-b-3xl'>
-                                    <p className='font-bold text-yellow-300 text-lg md:text-xl lg:text-2xl'>
+                                <div className="absolute left-0 right-0 grid place-items-center p-4 text-center h-[30%] bg-purple-900/80 backdrop-blur-3xl w-full bottom-0 rounded-b-3xl">
+                                    <p className="font-bold text-yellow-300 text-lg md:text-xl lg:text-2xl">
                                         {card.title}
                                     </p>
-                                    <p className='font-normal text-sm md:text-base lg:text-lg text-neutral-200'>
+                                    <p className="font-normal text-sm md:text-base lg:text-lg text-neutral-200">
                                         {card.description}
-                                        <span className='font-bold text-purple-100'>
+                                        <span className="font-bold text-purple-100">
                                             {card.description_Highlighted}
                                         </span>
                                     </p>
@@ -105,13 +106,13 @@ const GetTalents: React.FC = () => {
                     ))}
                 </div>
 
-                <div className='text-center mt-8 flex flex-col sm:flex-row gap-4 justify-center'>
-                    <Link href='/Pricing' passHref>
-                        <div className='h-12 w-48 md:h-16 md:w-64'>
+                <div className="text-center mt-8 flex flex-col sm:flex-row gap-4 justify-center">
+                    <Link href="/Pricing" passHref>
+                        <div className="h-12 w-48 md:h-16 md:w-64">
                             <Button CTAtext={ctaApplyText} />
                         </div>
                     </Link>
-                    <div className='h-12 w-48 md:h-16 md:w-64'>
+                    <div className="h-12 w-48 md:h-16 md:w-64">
                         <ButtonLite CTAtext={ctaReadMoreText} />
                     </div>
                 </div>
