da9ab76ea8153f05ca5e821ec3d39d978e2f522d
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-21 12:12:52 +0300

    refine the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,158 +1,275 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../../public/Identity/logotext.png";
 import ButtonComponent from "./Micros/Button";
+import logoImg from "@/../public/Identity/Images/2023/Group 976.png";
+const YouTubeIcon = () => (
+  <svg
+    className='aspect-square w-full'
+    version='1.1'
+    id='Layer_1'
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+    viewBox='0 0 461.001 461.001'
+    xmlSpace='preserve'
+    fill='#000000'>
+    <g id='SVGRepo_bgCarrier' strokeWidth={0} />
+    <g
+      id='SVGRepo_tracerCarrier'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <g id='SVGRepo_iconCarrier'>
+      {" "}
+      <g>
+        {" "}
+        <path
+          style={{ fill: "#ffffff" }}
+          d='M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728 c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137 C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607 c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z'
+        />{" "}
+      </g>{" "}
+    </g>
+  </svg>
+);
 
-const Footer = () => {
+const GitHubIcon = () => (
+  <svg
+    className='aspect-square w-full'
+    viewBox='0 0 20 20'
+    version='1.1'
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+    fill='#000000'>
+    <g id='SVGRepo_bgCarrier' strokeWidth={0} />
+    <g
+      id='SVGRepo_tracerCarrier'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <g id='SVGRepo_iconCarrier'>
+      {" "}
+      <title>github [#ffffff]</title> <desc>Created with Sketch.</desc>{" "}
+      <defs> </defs>{" "}
+      <g
+        id='Page-1'
+        stroke='none'
+        strokeWidth={1}
+        fill='none'
+        fillRule='evenodd'>
+        {" "}
+        <g
+          id='Dribbble-Light-Preview'
+          transform='translate(-140.000000, -7559.000000)'
+          fill='#ffffff'>
+          {" "}
+          <g id='icons' transform='translate(56.000000, 160.000000)'>
+            {" "}
+            <path
+              d='M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399'
+              id='github-[#ffffff]'>
+              {" "}
+            </path>{" "}
+          </g>{" "}
+        </g>{" "}
+      </g>{" "}
+    </g>
+  </svg>
+);
+
+const LinkedInIcon = () => (
+  <svg
+    className='aspect-square w-full'
+    viewBox='0 0 24 24'
+    xmlns='http://www.w3.org/2000/svg'
+    fill='#ffffff'
+    stroke='#ffffff'>
+    <g id='SVGRepo_bgCarrier' strokeWidth={0} />
+    <g
+      id='SVGRepo_tracerCarrier'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <g id='SVGRepo_iconCarrier'>
+      {" "}
+      <path d='M19,3a2,2,0,0,1,2,2V19a2,2,0,0,1-2,2H5a2,2,0,0,1-2-2V5A2,2,0,0,1,5,3H19m-.5,15.5V13.2a3.26,3.26,0,0,0-3.26-3.26h0a2.9,2.9,0,0,0-2.32,1.3V10.13H10.13V18.5h2.79V13.57a1.4,1.4,0,1,1,2.79,0V18.5H18.5M6.88,8.56A1.68,1.68,0,0,0,8.56,6.88h0a1.69,1.69,0,1,0-3.37,0h0A1.69,1.69,0,0,0,6.88,8.56M8.27,18.5V10.13H5.5V18.5Z' />{" "}
+    </g>
+  </svg>
+);
+const CurrentYear = new Date().getFullYear();
+const LINKS = [
+  {
+    title: "Solutions",
+    links: [
+      { url: "#", title: "For Students", status: true },
+      { url: "#", title: "Students activity", status: true },
+      { url: "#", title: "Be Job Ready", status: true },
+      { url: "#", title: "For Partners", status: true },
+      { url: "#", title: "Events Sponsor", status: true },
+      { url: "#", title: "Software Outsourcing", status: true },
+      { url: "#", title: "Get Talents", status: true },
+      { url: "#", title: "intern program managmenet", status: true },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { url: "/", title: "Blog", status: false },
+      { url: "/", title: "Students Use Cases", status: false },
+      { url: "/", title: "Partners Use Cases", status: false },
+      { url: "/", title: "Help Center", status: false },
+      { url: "/", title: "Material", status: false },
+      { url: "/Contact", title: "Contact Us", status: true },
+      { url: "/", title: "Support", status: false },],
+  },
+  {
+    title: "Helpful Links",
+    links: [
+      {
+        url: "/",
+        title: "Dragon's OpenSource",
+        status: false,
+      },
+      {
+        url: "/",
+        title: "Spotalents.com",
+        status: false,
+      },
+   
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      { url: "/website/privacy-policy", title: "Privacy Policy", status: false },
+      {
+        url: "/website/terms-and-conditions",
+        title: "Terms and Conditions",
+        status: true,
+      },
+      { url: "/website/cookie-policy", title: "Cookie Policy", status: false },
+      { url: "/website/cookie-policy", title: "User Agreement", status: false },
+      { url: "/website/", title: "Partnership Agreement", status: false },
+    ],
+  },
+];
+const SocialMediaLinks = [
+  {
+    url: "https://www.youtube.com/@Neferdata",
+    title: "YouTube",
+    icon: YouTubeIcon,
+  },
+  {
+    url: "https://github.com/neferdata",
+    title: "GitHub",
+    icon: GitHubIcon,
+  },
+  {
+    url: "https://www.linkedin.com/company/neferdata/",
+    title: "LinkedIn",
+    icon: LinkedInIcon,
+  },
+];
+
+function Footer() {
   return (
-    <footer className="mt-4 min-h-full flex flex-col justify-center items-center h-full">
-      <div className="flex justify-between min-h-[70vh] p-8 bg-center bg-no-repeat flex-wrap">
-        <div className="md:w-1/4 w-full px-2">
-          <Image
-            src={logo}
-            alt="Logo"
-            className="mb-4"
-            width={200}
-            height={200}
-          />
-          <h2 className="text-white font-bold text-2xl">Ready for Sprint?</h2>
-          <p className="text-white mt-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
-            iusto officia vitae sed illo sunt similique soluta perspiciatis
-            quisquam dolor possimus ab, nobis voluptatem ratione explicabo
-            numquam, accusamus autem debitis!
-          </p>
-        </div>
-        <div className="md:w-1/4 w-full px-2 text-center">
-          <h2 className="text-white font-bold text-2xl mb-4">Pages</h2>
-          <ul>
-            <li className="mt-2">
-              <Link href="#" className="text-[#80AB9E]">
-                Page1
-              </Link>
-            </li>
-            <li className="mt-2">
-              <Link href="#" className="text-[#80AB9E]">
-                Page2
-              </Link>
-            </li>
-            <li className="mt-2">
-              <Link href="#" className="text-[#80AB9E]">
-                Page3
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <footer className='bg-purple-800/20 rounded-t-[30px] overflow-hidden w-full relative'>
+      <div className=' h-full w-full bg-slate-950'>
+        <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
+      </div>
+      <div className='mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8'>
+        <div className='grid grid-cols-1 relative w-full gap-8 lg:grid-cols-3'>
+          <div className='relative w-full z-10'>
+            <div className='text-white w-auto'>
+              <div className='flex items-center -ml-7 -mt-4 w-auto'>
+                <Link
+                  href='/'
+                  className='flex hover:animate-pulse items-center  cursor-pointer hover:bg-gray-900/30 duration-700 rounded-2xl w-auto pt-4'>
+                  <Image
+                    src={logoImg}
+                    alt='Dragons Bootcamp LLC'
+                    className='aspect-video object-contain w-40 h-12 p-2 rounded-3xl px-4 hover:bg-black/20 duration-500 '
+                  />
+                </Link>
+              </div>
+            </div>
 
-        <div className="md:w-1/4 w-full px-2 text-center">
-          <h2 className="text-white font-bold text-2xl mb-4">Legal</h2>
-          <ul>
-            <li className="mt-2">
-              <Link href="/privacy-policy" className="text-[#80AB9E]">
-                Privacy Policy
-              </Link>
-            </li>
-            <li className="mt-2">
-              <Link href="/terms-of-service" className="text-[#80AB9E]">
-                Terms of Service
-              </Link>
-            </li>
-            <li className="mt-2">
-              <Link href="/cookie-policy" className="text-[#80AB9E]">
-                Cookie Policy
-              </Link>
-            </li>
-          </ul>
-        </div>
+            <p className='max-w-xs mt-2 font-bold text-white'>
+            Prepare for the future. Just say the magic word: ready for a sprint?
+                        </p>
+            <p className='relative mt-4 block'>Address:</p>
+            <p className='relative block'>
+              USA Inc. (HQ), 1309 Coffeen Avenue, STE 1200, Sheridan, WY 82801,
+              USA
+            </p>
 
-        <div className="md:w-1/4 w-full px-2 text-center">
-          <h2 className="text-white font-bold text-2xl mb-4">Social Media</h2>
-          <div className="flex justify-center gap-4">
-            <p>
-              <Link href="https://github.com/TutTrue">
-                <span className="border-2 inline-block border-white text-white p-2 rounded-full">
-                  <svg
-                    class="text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="1rem"
-                    height="1rem"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </Link>
+            <p className='mt-4 max-w-xs text-white font-bold'>
+              <span className='relative '>Reach through our social Media</span>{" "}
+              <br />
             </p>
-            <p>
-              <Link href="#">
-                <span className="border-2 inline-block border-white text-white p-2 rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="1rem"
-                    height="1rem"
-                    viewBox="0 0 48 48"
-                  >
-                    <path
-                      fill="#0288D1"
-                      d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
-                    ></path>
-                    <path
-                      fill="#FFF"
-                      d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
-                    ></path>
-                  </svg>{" "}
-                </span>
-              </Link>
-            </p>
-            <p>
-              <Link href="#">
-                <span className="border-2 inline-block border-white text-white p-2 rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="1rem"
-                    height="1rem"
-                    viewBox="0 0 48 48"
-                  >
-                    <path
-                      fill="#039be5"
-                      d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
-                    ></path>
-                    <path
-                      fill="#fff"
-                      d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
-                    ></path>
-                  </svg>{" "}
-                </span>
-              </Link>
-            </p>
+
+            {/* Social media */}
+            <ul className='mt-4 w-full justify-left flex gap-6'>
+              {SocialMediaLinks.map(({ url, title, icon: Icon }, idx) => (
+                <li key={idx}>
+                  <a
+                    href={url}
+                    rel='noreferrer'
+                    target='_blank'
+                    className='text-gray-200 transition'>
+                    <span className='sr-only select-none'>{title}</span>
+                    <span className='w-6 inline-block hover:!-rotate-6 hover:text-blue-700 hover:!scale-150 transition-all'>
+                      <Icon />
+                    </span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='grid grid-cols-1 w-[100%] z-10 right-0 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4'>
+            {LINKS.map(({ title, links }, idx) => (
+              <div key={idx}>
+                <p className='font-medium text-2xl text-gray-50 select-none'>
+                  {title}
+                </p>
+                <ul className='mt-6 space-y-4 select-none relative h-full w-full text-sm'>
+                  {links &&
+                    links.map((link, index) => (
+                      <li key={index}>
+                        {link.status && link.url ? (
+                          link.url.startsWith("http") ? (
+                            <a
+                              href={link.url}
+                              className='text-gray-200 transition'>
+                              {link.title}
+                            </a>
+                          ) : (
+                            <Link
+                              href={link.url}
+                              className='text-gray-200 transition'>
+                              {link.title}
+                            </Link>
+                          )
+                        ) : (
+                          <>
+                            <span className='text-gray-200'>{link.title}</span>
+                            <span className='soon text-xs rounded-full bg-purple-800 mx-2 py-[2px] px-[4px]'>Soon</span>
+                          </>
+                        )}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-      <Link href="/Pricing" passHref>
-        <div className="h-16 w-64">
-          <ButtonComponent CTAtext={"Let's Get Started"} />
-        </div>
-      </Link>
-      <div className="text-white text-center mt-8">
-        <p>All rights reserved@2024</p>
-        <p className="flex justify-center items-center gap-2">
-          Powered by Dragonsbootcampllc
+
+        <p className='text-xs font-bold text-gray-50'>
+          © {CurrentYear}.Dragons Bootcamp LLC. All rights reserved.
         </p>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
