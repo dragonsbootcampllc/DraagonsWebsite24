caed340fd77f5db298ff743c7807847f80edbc9f
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-22 21:57:16 +0300

    refactor: Update navbar icons and images for Solutions and Talents tabs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,77 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import React from "react";
-import Footer from "../components/Footer";
-const HeaderComp = dynamic(() => import("../components/Navbar/Navbar"), {
-  ssr: false,
-});
-const Head = dynamic(() => import("next/head"), {
-  ssr: false,
-});
+const Footer = dynamic(() => import("@/components/Footer"));
+
+import HeaderComp from "@/components/Navbar/Navbar";
+import Head from "next/head";
+
 export default function App({ Component, pageProps }: AppProps) {
-  //TODO: Change and add (Tracks Page(Instade of home), Blog)
-  let nav_links = [
-    {
-      id: 1,
-      name: "Talents",
-      link: "/Talents",
-    },
-    {
-      id: 2,
-      name: "Team",
-      link: "/Team",
-    },
-    {
-      id: 3,
-      name: "Gallery",
-      link: "/Gallery",
-    },
-    {
-      id: 4,
-      name: "DSA",
-      link: "/DSA",
-    },
-    {
-      id: 5,
-      name: "Bootcamp",
-      link: "/Bootcamp",
-    },
-    {
-      id: 6,
-      name: "JopReady",
-      link: "/JopReady",
-    },
-    {
-      id: 7,
-      name: "students",
-      link: "/students",
-    },
-  ];
-
-  let Join = [
-    {
-      id: 1,
-      name: "Contact Us ",
-      link: "/Contact",
-    },
-  ];
-
   return (
     <>
       <div>
-        <Head>
-          <title>Dragons</title>
-          <meta
-            property="og:Dragons"
-            content="Dragons is a community of talented developers, designers, and product managers who are passionate about building products that people love."
-            key="Dragons is a community of talented developers, designers, and product managers who are passionate about building products that people love."
-          />
-
-          <link
-            rel="shortcut icon"
-            href="/Identity/Images/2023/SVG/LLogo.svg"
-          />
-        </Head>
+        <HeadSEO />
 
         <HeaderComp />
 
@@ -85,3 +24,27 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+const HeadSEO = () => {
+  return (
+    <Head>
+      <title>Dragons</title>
+      <meta
+        property='og:Dragons'
+        content='Dragons is a community of talented developers, designers, and product managers who are passionate about building products that people love.'
+        key='description'
+      />
+      <meta
+        property='og:description'
+        content='Dragons is a community of talented developers, designers, and product managers who are passionate about building products that people love.'
+      />
+      <meta
+        name='keywords'
+        content='Dragons, developers, designers, product managers, community, tech'
+      />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='robots' content='index, follow' />
+      <link rel='shortcut icon' href='/Identity/Images/2023/SVG/LLogo.svg' />
+    </Head>
+  );
+};
