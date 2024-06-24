6f22850f5bd0e0557708d9c7af8b41e7471faba3
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-24 12:34:16 +0300

    fix mobile route view

--- src/pages/blogs/layout.tsx
+++ src/pages/blogs/layout.tsx
@@ -32,36 +32,40 @@ const BlogLayout = ({ children }: { children: ReactNode }) => {
     };
 
     return (
-        <div className='grid place-items-center'>
-                <div className="flex min-h-screen w-full pt-24 max-w-[1500px]">
-                        <div className="flex  min-h-screen w-full h-[calc(100vh-5.5rem)]">
-                                <BlogSidebar />
-                                <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#030014_40%,#63e_100%)]"></div>
-                                <div className="w-full box-border overflow-hidden flex flex-col">
-                                        <div className="w-full select-none h-24 text-gray-400 box-border flex items-center text-2xl pl-10 gap-4 font-semibold">
-                                                {currentUrlEnds().map(({ title, url }, index, arr) => (
-                                                        <span key={index} className="flex items-center">
-                                                                <Link href={url} passHref>
-                                                                        <span className={`cursor-pointer transition-all hover:text-blue-1 ${index + 1 === arr.length ? 'text-slate-200' : 'text-gray-500'}`}>
-                                                                                {title}
-                                                                        </span>
-                                                                </Link>
-                                                                {index + 1 !== arr.length && (
-                                                                        <span className={`transition-all ${index + 1 === arr.length ? 'text-slate-800' : 'text-gray-400'}`}>
-                                                                                &gt;
-                                                                        </span>
-                                                                )}
-                                                        </span>
-                                                ))}
-                                        </div>
-
-                                        <div className="h-full  overflow-hidden overflow-y-auto box-border w-full flex justify-center items-center">
-                                                {children}
-                                        </div>
-                                </div>
-                        </div>
+      <div className='grid relative place-items-center'>
+        <div className='flex min-h-screen w-full relative pt-24 max-w-[1500px]'>
+          <div className='flex relative min-h-screen w-full h-[calc(100vh-5.5rem)]'>
+            <BlogSidebar />
+            <div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#030014_40%,#63e_100%)]'></div>
+            <div className='w-full relative   px-2 md:px-10 grid  place-items-center '>
+              <div className='w-full  min-h-24 select-none  text-gray-400  box-border flex items-center md:text-2xl mb-10 md:m-0   gap-4 font-semibold'>
+              <div className='absolute top-0'>
+              {currentUrlEnds().map(({ title, url }, index, arr) => (
+                  <span key={index} className='flex items-center'>
+                    <Link href={url} passHref>
+                      <span
+                        className={`cursor-pointer transition-all hover:text-blue-1 ${index + 1 === arr.length ? "text-slate-200" : "text-gray-500"}`}>
+                        {title}
+                      </span>
+                    </Link>
+                    {index + 1 !== arr.length && (
+                      <span
+                        className={`transition-all ${index + 1 === arr.length ? "text-slate-800" : "text-gray-400"}`}>
+                        &gt;
+                      </span>
+                    )}
+                  </span>
+                ))}
                 </div>
+              </div>
+
+              <div className='min-h-full h-full  box-border w-full flex justify-center items-center'>
+                {children}
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
