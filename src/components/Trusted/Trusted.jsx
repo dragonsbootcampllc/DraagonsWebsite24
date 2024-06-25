fa98bb68041e5c874341cf74810b4cf02a840d1a
devbn3li <fudwhc@gmail.com>
2024-06-26 00:59:46 +0300

    Dragons Student Activities

--- src/components/Trusted/Trusted.jsx
+++ src/components/Trusted/Trusted.jsx
@@ -12,23 +12,19 @@ import { SiWpengine } from "react-icons/si";
 import { FaMicrosoft } from "react-icons/fa";
 import { FaAmazon } from "react-icons/fa";
 
-
 const brands = [
-    { id: 1, name: "DigitalOcean", img: "/Brands/DigitalOcean.svg" },
-    { id: 2, name: "Neferdata", img: "/Brands/neferdata-logo.png" },
-    { id: 4, name: "JetBrains", img: "/Brands/JetBreans.svg" },
-    { id: 5, name: "Vercel", img: "/Brands/Vercel.svg" },
-    { id: 6, name: "WP", img: "/Brands/WP-2.svg" },
-    { id: 7, name: "Udacity", img: "/Brands/udacity.svg" },
-    { id: 8, name: "Namecheap", img: "/Brands/Namecheap.svg" },
-    
-  ];
+  { id: 1, name: "DigitalOcean", img: "/Brands/DigitalOcean.svg" },
+  { id: 2, name: "Neferdata", img: "/Brands/neferdata-logo.png" },
+  { id: 4, name: "JetBrains", img: "/Brands/JetBreans.svg" },
+  { id: 5, name: "Vercel", img: "/Brands/Vercel.svg" },
+  { id: 6, name: "WP", img: "/Brands/WP-2.svg" },
+  { id: 7, name: "Udacity", img: "/Brands/udacity.svg" },
+  { id: 8, name: "Namecheap", img: "/Brands/Namecheap.svg" },
+];
 
 const renderLogos = (brands) => {
   return brands.map((brand) => (
-    <li key={brand.id}>
-      {renderIcon(brand.name)}
-    </li>
+    <li key={brand.id}>{renderIcon(brand.name)}</li>
   ));
 };
 
@@ -63,7 +59,8 @@ export default function Trusted() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
       if (scrollTop + clientHeight >= scrollHeight) {
         // Load more brands
         const nextBrands = brands.slice(0, loadedBrands.length + 10);
@@ -79,21 +76,19 @@ export default function Trusted() {
 
   return (
     <>
-      <div className="relative font-inter h-32  grid place-items-center antialiased">
-      <div className={styles.sec3_main_container_title}>
+      <div className="relative font-inter  grid place-items-center antialiased">
+        <div className={styles.sec3_main_container_title}>
           Trusted by 70+ Partners
         </div>
 
         <div className={styles.boujee_text}>
           <p>Thank You For Trusting Us</p>
         </div>
-        <main className="relative h-20 flex flex-col justify-center max-w-[1400px] overflow-hidden">
+        <main className="relative flex flex-col justify-center max-w-[1400px] overflow-hidden">
           <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-24">
             <div className="text-center">
               {/* Logo Carousel animation */}
-              <div
-                className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
-              >
+              <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
                 <ul
                   ref={logosRef}
                   className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none "
