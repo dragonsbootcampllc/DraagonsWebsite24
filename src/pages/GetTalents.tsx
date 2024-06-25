7f6941e1ed06079fa6ef48c4b61b595ee4f75531
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-26 01:39:35 +0300

    refactor: Update styles and remove unused code in CTAMaterial, TechStack, BootcampPage, Home, CanvaReveal, ThoughtLeader, InfiniteMovingCards, and CardsMeteors components

--- src/pages/GetTalents.tsx
+++ src/pages/GetTalents.tsx
@@ -21,8 +21,8 @@ const HeroObj = {
 
 const GetTalentsPage: React.FC = () => {
     return (
-        <div className=" ">
-            <div className="flex flex-col gap-[90px] bg-fixed bg-cover mt-10 h-full w-full relative">
+        <div className="  grid place-items-center  relative">
+            <div className="flex flex-col max-w-[1400px] gap-[90px] bg-fixed bg-cover mt-10 h-full w-full relative">
                 <div className="absolute bg-fixed z-[-1] inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:80px_80px] [mask-image:radial-gradient(ellipse_70%_100%_at_70%_10%,_#000_60%,_transparent_100%)]"></div>
                 <Hero
                     News={HeroObj.News}
