cfa8dfeca488fdc5f7c0bb78a44f2d7eb8447bdc
DarkCode662432 <ff9371668@gmail.com>
2024-06-26 20:25:09 +0300

    [UI] Discovery page

---
+++ src/components/Cards/MinisprintCard.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ButtonComponent from '../Micros/Button';
+import ButtonLite from '../Micros/ButtonLite';
+import { Date_1, Time_1 } from '../Icons';
+
+interface MinisprintCardProps {
+    price: string;
+    title: string;
+    description: string;
+    imageUrl: string;
+    duration: string;
+    startDate: string;
+    exploreUrl: string;
+}
+
+const truncateDescription = (description: string, maxLength: number) => {
+    if (description.length > maxLength) {
+        return description.substring(0, maxLength) + '...';
+    }
+    return description;
+};
+
+const MinisprintCard: React.FC<MinisprintCardProps> = ({ price, title, description, imageUrl, duration, startDate, exploreUrl }) => {
+    const maxDescriptionLength = 100;
+    const BASE_URL = '/minisprints';
+
+    return (
+        <div className="max-w-sm rounded-xl relative overflow-hidden shadow-lg cursor-pointer bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all group">
+            <div className='w-full h-64 flex justify-center items-center overflow-hidden'>
+                <img className="min-w-full min-h-full object-cover group-hover:scale-110 group-hover:rotate-6 transition-all" src={imageUrl} alt={title} />
+            </div>
+            <div className='p-4 flex flex-col gap-4'>
+                <div>
+                    <div className="font-bold text-xl mb-2">{title}</div>
+                    <p className="text-slate-300 text-base">
+                        {truncateDescription(description, maxDescriptionLength)}
+                    </p>
+                </div>
+                <div>
+                    <p className="text-slate-100 font-semibold rotate-45 absolute top-2 -right-11 py-1 bg-yellow-500 border-y border-yellow-500/40 w-[150px] text-center text-lg shadow-lg">
+                        {price}
+                        <span className='absolute inset-0 overflow-hidden rounded-full'>
+                            <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] transition-opacity duration-500 opacity-100 rotate-180 group-hover:animate-pulse' />
+                        </span>
+                    </p>
+                    <p className="text-slate-300 flex"><span className="mr-1 w-5 aspect-square"><Time_1 /></span> Duration: {duration}</p>
+                    <p className="text-slate-300 flex"><span className="mr-1 w-5 aspect-square"><Date_1 /></span> Start Date: {startDate}</p>
+                </div>
+                <div className="flex justify-end gap-3">
+                    <ButtonLite CTAtext='Explore' className='!w-fit' type='link' href={`${BASE_URL}/${exploreUrl}`} />
+                    <ButtonComponent CTAtext='Buy' className='!w-fit' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MinisprintCard;
--- src/components/Micros/Button.tsx
+++ src/components/Micros/Button.tsx
@@ -1,25 +1,47 @@
+import Link from "next/link";
 import React from "react";
 
 interface ButtonComponentProps {
   CTAtext: string;
   className?: string;
-
+  type?: 'button' | 'link'; // Specify possible types
+  href?: string; // href is required if type is 'link'
 }
 
-const ButtonComponent: React.FC<ButtonComponentProps> = ({ CTAtext,...props }) => {
-  return (
-    <button className='relative animate-shimmer bg-[linear-gradient(110deg,#7B1FA2,45%,#CE93D8,55%,#7B1FA2)] bg-[length:200%_100%] bg-purple-700 group cursor-pointer shadow-2xl rounded-full p-px text-white font-bold leading-6 inline-block w-full h-full'>
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  CTAtext,
+  type = 'button', // Default to 'button'
+  href,
+  className,
+  ...props
+}) => {
+  const buttonClassName = `relative animate-shimmer bg-[linear-gradient(110deg,#7B1FA2,45%,#CE93D8,55%,#7B1FA2)] bg-[length:200%_100%] bg-purple-700 group/buttonComponent cursor-pointer shadow-2xl rounded-full p-px text-white font-bold leading-6 inline-block w-full h-full ${className}`;
+
+  return type === 'link' && href ? (
+    <Link href={href} className={buttonClassName} {...props}>
       <span className='absolute inset-0 overflow-hidden rounded-full'>
-        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100 rotate-180' />
+        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover/buttonComponent:opacity-100 rotate-180' />
       </span>
       <div className='relative flex items-center justify-center h-full w-full z-10 rounded-full py-2 px-4 ring-1 ring-white/10 text-base sm:text-lg md:text-xl'>
-      <span className={props.className ? props.className : "text-white"}>
+        <span>
           {CTAtext}
         </span>
       </div>
-      <span className='absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] animate-pulse bg-gradient-to-r from-emerald-400/0 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40' />
+      <span className='absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] animate-pulse bg-gradient-to-r from-emerald-400/0 to-emerald-400/0 transition-opacity duration-500 group-hover/buttonComponent:opacity-40' />
+    </Link>
+  ) : (
+    <button type="button" className={buttonClassName} {...props}>
+      <span className='absolute inset-0 overflow-hidden rounded-full'>
+        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover/buttonComponent:opacity-100 rotate-180' />
+      </span>
+      <div className='relative flex items-center justify-center h-full w-full z-10 rounded-full py-2 px-4 ring-1 ring-white/10 text-base sm:text-lg md:text-xl'>
+        <span>
+          {CTAtext}
+        </span>
+      </div>
+      <span className='absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] animate-pulse bg-gradient-to-r from-emerald-400/0 to-emerald-400/0 transition-opacity duration-500 group-hover/buttonComponent:opacity-40' />
     </button>
   );
-}
+};
 
 export default ButtonComponent;
