1cc7c363f2571d4670db354048c5bde12167ec45
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-27 05:55:47 +0300

    refactor: Update FAQ page styles and remove unused code

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -1,5 +1,6 @@
 import ButtonComponent from "@/components/Micros/Button";
 import React, { useEffect, useState, ChangeEvent } from "react";
+import { motion } from 'framer-motion';
 
 interface SearchResult {
   title: string;
@@ -41,32 +42,49 @@ const MyHelpCenter: React.FC = () => {
   };
 
   return (
-    <div className='w-full text-center p-2 my-12'>
-      <div className='text-3xl  flex-col content-center max-w-4xl text-center md:text-5xl z-10 flex justify-center m-auto relative mb-3 text-gray-200 font-semibold'>
-        <h1 className='md:text-xl text-center text-lg font-medium text-purple-800 uppercase'>
-          Welcome to the Help Center
-        </h1>
-        <h1>How can we help you?</h1>
-      </div>
-      <div className='text-3xl md:text-xl text-center max-w-[1000px] gap-4 z-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center m-auto relative mb-3 p-5 text-gray-200 font-semibold'>
-        {searchResults.map((result, index) => (
-          <div key={index} className='w-full grid place-items-center  sm:w-auto mx-2 mb-4'>
-            <div className='bg-purple-800/20 max-w-[400px] aspect-auto grid place-items-center p-4 shadow rounded-3xl'>
-              <div>
-                <h2 className='text-gray-200 text-lg sm:text-xl'>
-                  {result.title}
-                </h2>
-                <p className='text-gray-400 text-sm'>{result.description}</p>
-
-                <div className='py-2'>
-                  <ButtonComponent CTAtext='Explore articles' />
-                </div>
+    <div className='w-full min-h-screen grid place-items-center text-center p-2 my-12'>
+   <div>
+   <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='text-3xl flex-col content-center max-w-4xl text-center md:text-5xl z-10 flex justify-center m-auto relative mb-3 text-gray-200 font-semibold'
+    >
+      <h1 className='md:text-xl text-center text-lg font-medium text-purple-800 uppercase'>
+        Welcome to the Help Center
+      </h1>
+      <h1>How can we help you?</h1>
+    </motion.div>
+    <div className='text-3xl md:text-xl text-center max-w-[1000px] gap-4 z-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center m-auto relative mb-3 p-5 text-gray-200 font-semibold'>
+      {searchResults.map((result, index) => (
+        <motion.div
+          key={index}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+          className='w-full grid place-items-center sm:w-auto mx-2 mb-4'
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className='bg-purple-800/20 max-w-[400px] aspect-auto grid place-items-center p-4 shadow rounded-3xl'
+          >
+            <div>
+              <h2 className='text-gray-200 text-lg sm:text-xl'>
+                {result.title}
+              </h2>
+              <p className='text-gray-400 text-sm'>{result.description}</p>
+              <div className='py-2'>
+                <ButtonComponent CTAtext='Explore articles' />
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          </motion.div>
+        </motion.div>
+      ))}
     </div>
+   </div>
+  </div>
   );
 };
 
