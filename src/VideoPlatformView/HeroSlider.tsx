252d563398c1018293885caaae778e8c26a3e0a0
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-06-30 08:52:39 +0300

    feat: Add Career Sprint real-world scenarios to minisprints page

---
+++ src/VideoPlatformView/HeroSlider.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React, { use } from "react";
+import CoverImage from "@/../public/45f.png";
+import ButtonComponent from "@/components/Micros/Button";
+import ButtonComponentLite from "@/components/Micros/ButtonLite";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function HeroSlider(props: { exploreUrl: string }) {
+  const { exploreUrl } = props;
+  const route = useRouter();
+  const  CurrentURL  = route.route;
+
+
+  return (
+    <div className='w-full h-full my-5 opacity-80 relative p-4 md:p-12 overflow-hidden'>
+      <div className='w-full xl:min-h-[600px] lg:min-h-[350px] md:min-h-[300px] sm:min-h-[250px]  h-[170px] relative rounded-3xl overflow-hidden'>
+        <Image
+          src={CoverImage}
+          className='scale-110'
+          alt='hero'
+          layout='fill'
+          objectFit='cover'
+        />
+        <div className='absolute left-0 flex flex-col gap-3 p-4 md:p-12 sm:p-12 px-2 sm:px-0 sm:pr-5 sm:top-[20%] bottom-0 -sm:max-w-[230px] text-left justify-center bg-opacity-75 '>
+          <div className='sm:max-w-md lg:max-w-3xl  text-center  sm:p-4 md:p-0 text-white sm:text-left'>
+            <h1 className='xl:text-7xl capitalize lg:text-3xl md:text-2xl sm:text-xl  font-bold'>
+              The Developer's Quest with Real-World Scenarios
+            </h1>
+            {/* Remove description on mobile */}
+            <p className='text-base py-2 lg:text-lg font-medium text-gray-200 hidden md:block'>
+              Understand career technical paths, with real-world scenarios and
+              intensive 2 weeks of learning, practice, and mentorship.
+            </p>
+            {/* Buttons (remove on mobile) */}
+            <div className='lg:flex gap-4 hidden mt-4 justify-left md:justify-start'>
+              <Link href={CurrentURL+"/"+exploreUrl} className='h-16 w-64' passHref>
+                <ButtonComponent CTAtext='Explore courses' />
+              </Link>
+              <div className='h-16 w-64'>
+                <ButtonComponentLite CTAtext='Buy Now' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/minisprints/index.tsx
+++ src/pages/minisprints/index.tsx
@@ -1,8 +1,9 @@
 import MinisprintCard, { MinisprintCardProps } from '@/components/Cards/MinisprintCard';
 import Hero_2 from '@/components/Hero/Hero_2';
-import React from 'react';
+import HeroSlider from '@/VideoPlatformView/HeroSlider';
+import CoverImage from "@/../public/45f.png";
 
-export default function index() {
+import React from 'react';
     const heroObj = {
         News: {
             id: 2,
@@ -18,102 +19,23 @@ export default function index() {
 
     const minisprintData = [
         {
-            price: "$99",
-            title: "Beginner's Guide to Python",
-            description: "Start your programming journey with Python, a versatile language used in various domains. This course covers basic to intermediate concepts.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "4 weeks",
-            startDate: "2024-07-01",
-            exploreUrl: 'python-course'
-        },
-        {
-            price: "$149",
-            title: "Advanced JavaScript Techniques",
-            description: "Dive deep into JavaScript and learn advanced techniques and patterns to write more efficient and maintainable code.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "6 weeks",
-            startDate: "2024-07-10",
-            exploreUrl: 'advanced-javascript'
-        },
-        {
-            price: "$129",
-            title: "Web Development Bootcamp",
-            description: "Become a full-stack web developer by mastering HTML, CSS, JavaScript, and backend technologies. Build real-world projects.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "8 weeks",
-            startDate: "2024-08-01",
-            exploreUrl: 'web-development-bootcamp'
-        },
-        {
-            price: "$199",
-            title: "Data Science with Python",
-            description: "Learn data analysis, visualization, and machine learning techniques using Python. Perfect for aspiring data scientists.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "10 weeks",
+            price: "$30",
+            title: "Career Sprint real-world scenarios",
+            description: "Understand career technical paths, with real-world scenarios and intensive 2 weeks of learning, practice, and mentorship.",
+            imageUrl: CoverImage.src,
+            duration: "2 weeks",
             startDate: "2024-07-15",
-            exploreUrl: 'data-science-python'
+            exploreUrl: 'CareerSprint',
         },
-        {
-            price: "$89",
-            title: "Introduction to SQL",
-            description: "Master the basics of SQL and database management. Learn how to query databases and manipulate data efficiently.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "3 weeks",
-            startDate: "2024-07-20",
-            exploreUrl: 'introduction-sql'
-        },
-        {
-            price: "$179",
-            title: "React.js for Beginners",
-            description: "Build dynamic web applications using React.js. Understand components, hooks, and state management in React.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "5 weeks",
-            startDate: "2024-08-05",
-            exploreUrl: 'reactjs-beginners'
-        },
-        {
-            price: "$109",
-            title: "Node.js and Express.js",
-            description: "Learn to build backend services and APIs using Node.js and Express.js. Ideal for full-stack developers.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "6 weeks",
-            startDate: "2024-08-10",
-            exploreUrl: 'nodejs-expressjs'
-        },
-        {
-            price: "$139",
-            title: "Introduction to Machine Learning",
-            description: "Discover the fundamentals of machine learning and build your first models using Python libraries such as scikit-learn.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "7 weeks",
-            startDate: "2024-08-20",
-            exploreUrl: 'introduction-machine-learning'
-        },
-        {
-            price: "$159",
-            title: "Building RESTful APIs",
-            description: "Learn how to design and build RESTful APIs with best practices and security in mind. Use Node.js, Express, and MongoDB.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "5 weeks",
-            startDate: "2024-09-01",
-            exploreUrl: 'building-restful-apis'
-        },
-        {
-            price: "$119",
-            title: "DevOps Fundamentals",
-            description: "Understand the core concepts of DevOps, including continuous integration, continuous deployment, and automation tools.",
-            imageUrl: "https://via.placeholder.com/150",
-            duration: "8 weeks",
-            startDate: "2024-09-10",
-            exploreUrl: 'devops-fundamentals'
-        }
     ];
 
-    return (
-        <div className='flex flex-col justify-center items-center mx-auto max-w-[1600px] mb-12'>
-            <Hero_2 {...heroObj} />
+export default function index() {
 
-            <div className='flex justify-center items-center flex-wrap w-11/12 gap-12'>
+    return (
+        <div className='flex flex-col  relative min-h-[screen] justify-center items-center mx-auto max-w-[2000px] mb-12'>
+            {/* <Hero_2 {...heroObj} /> */}
+            <HeroSlider exploreUrl={minisprintData[0].exploreUrl}/>
+            <div className='flex justify-center min-h-[400px] items-center flex-wrap w-11/12 gap-12'>
                 {minisprintData.length !== 0 ? 
                     minisprintData.map((minisprint, i) => <MinisprintCard key={i} {...minisprint} />)
                     :
