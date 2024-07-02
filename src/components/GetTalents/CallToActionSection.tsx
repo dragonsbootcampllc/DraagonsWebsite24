d2bb096a69ab71a8b615836a1c2e49715b08cf7b
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-07-02 14:24:16 +0300

    Refactor CanvaReveal and AnimatedTooltip components

--- src/components/GetTalents/CallToActionSection.tsx
+++ src/components/GetTalents/CallToActionSection.tsx
@@ -7,7 +7,6 @@ import talents from "@/pages/json/Team2023.json";
 import { Talent } from "../HOOKs/useTransformTalentData";
 import { MdGroups } from "react-icons/md";
 
-
 import useTransformTalentData from "../HOOKs/useTransformTalentData";
 import ButtonComponent from "../Micros/Button";
 import ButtonLiteComponent from "../Micros/ButtonLite";
@@ -18,14 +17,15 @@ import Image from "next/image";
 const ctaHeadline: string = "Ready to";
 const heroHighlightedText: string = " Elevate";
 const ctaSubHeadline: string = " Your Team?";
-const description: string = "Join us and take advantage of our comprehensive support services designed to ensure your team's success. Get started today and see the difference with our HRMS.";
+const description: string =
+  "Join us and take advantage of our comprehensive support services designed to ensure your team's success. Get started today and see the difference with our HRMS.";
 const ctaApplyText: string = "Apply Now";
 const ctaBookCallText: string = "Book 1:1 Call";
 const transformedTalents = useTransformTalentData(talents as Talent[]);
 
 const CallToActionComponent: React.FC = () => {
   return (
-    <div className='py-12 text-white ' id="CallToActionComponent">
+    <div className='py-12 text-white ' id='CallToActionComponent'>
       <div className='container mx-auto max-w-[1400px] flex flex-col gap-6 items-center text-center px-4'>
         <div className='w-full flex justify-center'>
           <div className='flex items-center justify-center bg-purple-700 w-[116px] h-[116px] p-2 shadow-2xl shadow-blue-700/40 rounded-[2rem]'>
@@ -35,8 +35,7 @@ const CallToActionComponent: React.FC = () => {
 
         <div
           style={{ fontFamily: "interV" }}
-          className='text-4xl md:text-6xl lg:text-7xl content-center max-w-4xl text-center z-10 flex flex-col justify-center m-auto relative mb-3 text-gray-200 font-semibold'
-        >
+          className='text-4xl md:text-6xl lg:text-7xl content-center max-w-4xl text-center z-10 flex flex-col justify-center m-auto relative mb-3 text-gray-200 font-semibold'>
           <h1>
             {ctaHeadline}
             <span className='bg-gradient-to-r from-purple-700 to-purple-900 text-transparent bg-clip-text'>
@@ -44,20 +43,22 @@ const CallToActionComponent: React.FC = () => {
             </span>
             {ctaSubHeadline}
           </h1>
-          <p className="text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8">
+          <p className='text-base sm:text-lg md:text-xl lg:text-2xl max-w-4xl mx-auto text-white/70 mt-4 mb-8'>
             {description}
           </p>
         </div>
-        <div className='max-w-[1400px]'>
-          <div className='flex-row hidden md:flex items-center justify-center mb-10 w-full'>
+        <div className='max-w-[1400px] '>
+          <div className='flex-row hidden md:flex relative h-24 mb-10 '>
             <AnimatedTooltip
-              items={transformedTalents as {
-                id: number;
-                name: string;
-                designation: string;
-                image: string;
-                link_profile_Linkedin: string;
-              }[]}
+              items={
+                transformedTalents as {
+                  id: number;
+                  name: string;
+                  designation: string;
+                  image: string;
+                  link_profile_Linkedin: string;
+                }[]
+              }
             />
           </div>
           <div className='mt-8 flex flex-col md:flex-row gap-4 justify-center'>
@@ -72,12 +73,10 @@ const CallToActionComponent: React.FC = () => {
               </div>
             </Link>
           </div>
-
         </div>
       </div>
     </div>
   );
 };
-
 
 export default CallToActionComponent;
--- src/pages/DragonsSprints/[slug].tsx
+++ src/pages/DragonsSprints/[slug].tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useRouter } from "next/router";
 import ButtonComponent from '@/components/Micros/Button';
 import { Correct_1, Document_1, Time_1 } from '@/components/Icons';
-
+import CourseHeasder from '@/pages/DragonsSprints/Components/CourseHeader';
 interface Coupon {
   name: string,
   discount: number
@@ -24,6 +24,14 @@ export default function minisprint_explore_view() {
     articles: '8',
     resources: '5'
   };
+   const Instructors =["General"]
+   const Category =["General"]
+   const Level =["Beginner"]
+   const Language =["Arabic","English"]
+   const HasCertificate = true
+
+
+
   const requirements = ['test test test test', 'test 1 test 1 test 1 test 1', 'test 2 test 2 test 2 test 2', 'test 3 test 3 test 3', 'test 4 test 4 test 4 test 4 test 4 test 4'];
 
   const add_coupon = (couponData: Coupon | null) => {
@@ -32,79 +40,18 @@ export default function minisprint_explore_view() {
   const price = 1250;
 
   return (
-    <div className="flex flex-col max-w-[1600px] items-center mx-auto mb-24 gap-24">
-      <div className='flex h-[500px] justify-between items-center'>
-        <div className="flex flex-col w-1/2 justify-center gap-12">
-          <div className='flex flex-col justify-center items-start gap-8'>
-            <h1 className='font-semibold text-6xl'>{minisprint_name}</h1>
-            <p className='font-semibold text-lg text-slate-300 w-11/12'>{description}</p>
-          </div>
-
-          <ButtonComponent CTAtext='Buy Now' className='!w-fit !h-fit' />
-        </div>
-
-        <div className='w-1/2 h-[400px] flex justify-center items-center'>
-          <div className="bg-gray-300/20 rounded-md h-4/5 w-1/2 text-gray-300 text-4xl font-bold flex flex-col justify-end gap-5 p-4">
-            {!coupon ?
-              <span className='flex justify-center h-full items-center'>
-                <span className='text-5xl font-bold'>${price}</span>
-              </span> :
-              <span className='flex justify-center h-full items-center relative'>
-                <span className='text-5xl font-bold relative'>
-                  ${price - (price * (coupon.discount / 100))}
-                </span>
-                <span className='text-xl font-bold line-through px-2 text-gray-300/40'>${price}</span>
-                <span className='text-xl text-yellow-500 animate-pulse hover:scale-105 transition-all'>{coupon.discount}%</span>
-              </span>
-            }
-            {
-              coupon && (
-                <span className='border-2 border-dashed rounded-md flex flex-col relative px-4 py-2 w-full'>
-                  <span className='text-2xl font-bold'>{coupon.name} <span className='font-normal'>is applied</span></span>
-                  <span className='absolute top-1/2 -translate-y-1/2 right-2 w-6 aspect-square cursor-pointer group/closeIcon' onClick={() => add_coupon(null)}>
-                    <span className="h-1 w-full rounded-full bg-white group-hover/closeIcon:rotate-45 transition-all absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></span>
-                    <span className="h-1 w-full rounded-full bg-white group-hover/closeIcon:-rotate-45 transition-all absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></span>
-                  </span>
-                </span>
-              )
-            }
-            <input type="text" className='inline-block p-2 text-lg text-black bg-blue-200 border-blue-500 transition-all shadow-[0_0_0_0px_rgba(13,119,252,0.5)] focus:shadow-[0_0_0_3px_rgba(13,119,252,0.5)] border rounded-lg focus:outline-none overflow-y-auto overflow-x-hidden w-full h-20' onChange={(e) => setCouponInput(e.target.value)} value={couponInput} />
-            <ButtonComponent CTAtext='Add' onClick={() => { couponInput.trim() && add_coupon({ name: couponInput, discount: 20 }); setCouponInput("") }} className='!h-fit' />
-          </div>
-        </div>
-      </div>
-
-      <div className='w-full flex flex-col gap-12'>
-        <h2 className='text-5xl font-semibold'>What you'll learn?</h2>
-        <div className='flex flex-col gap-5'>
-          {
-            benefits.map((benefit, index) => (
-              <span key={index} className='flex text-slate-200 text-2xl items-center justify-start'><span className='mr-2 w-5 aspect-square'><Correct_1 /></span><span>{benefit}</span></span>
-            ))
-          }
-        </div>
-      </div>
-
-      <div className='w-full flex flex-col gap-12'>
-        <h2 className='text-5xl font-semibold'>This course includes:</h2>
-        <div className='flex flex-col gap-5'>
-          <span className='flex text-slate-200 text-2xl items-center justify-start'><span className='mr-2 w-5 aspect-square'><Time_1 /></span><span>{content.duration}</span></span>
-          <span className='flex text-slate-200 text-2xl items-center justify-start'><span className='mr-2 w-5 aspect-square'><Document_1 /></span><span>{content.assignments} Assignments</span></span>
-          <span className='flex text-slate-200 text-2xl items-center justify-start'><span className='mr-2 w-5 aspect-square'>O</span><span>{content.articles} Articles</span></span>
-          <span className='flex text-slate-200 text-2xl items-center justify-start'><span className='mr-2 w-5 aspect-square'>O</span><span>{content.resources} Resources</span></span>
-        </div>
-      </div>
-
-      <div className='w-full flex flex-col gap-12'>
-        <h2 className='text-5xl font-semibold'>Requirements</h2>
-        <div className='flex flex-col gap-5'>
-          {
-            requirements.map((requirement, index) => (
-              <span key={index} className='flex text-slate-200 text-2xl justify-start items-center'><span className='mr-1 text-4xl'>•</span><span>{requirement}</span></span>
-            ))
-          }
-        </div>
-      </div>
+    <div className=" h-full w-full flex justify-center">
+     <CourseHeasder
+      title={minisprint_name}
+      description={description}
+      duration={content.duration}
+      Instructors = {Instructors}
+      Category = {Category}
+      Level = {Level}
+      Language = {Language}
+      HasCertificate = {HasCertificate}
+     />
+     
     </div>
   )
 }
