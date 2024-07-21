2975a9c820d9f927a2da797e91b93ee5508ac4d6
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-07-21 19:03:19 +0300

    update

--- src/pages/DragonsSprints/blogs/[...slug].tsx
+++ src/pages/DragonsSprints/blogs/[...slug].tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import React from "react";
 import Layout from "./layout";
-import { CategoryView, Blog as BlogType } from "./components/CategoryView";
+import CategoryView from "./components/CategoryView";
+import { Blog as BlogType } from "./components/CategoryView";
 import {
   BASE_URL,
   useBlog,
