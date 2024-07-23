46c75cb2d170d199c030776142cc64e608620938
DarkCode662432 <ff9371668@gmail.com>
2024-07-23 16:25:56 +0300

    Revert "[enhance]"
    This reverts commit d0ded798d00d7175cfbe18543eee69f1ec36311d.

--- src/pages/DragonsSprints/[slug]/Components/card-hover-effectv2.tsx
+++ src/pages/DragonsSprints/[slug]/Components/card-hover-effectv2.tsx
@@ -33,7 +33,7 @@ const HoverEffect = ({
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
           style={
-            0 === item.link.length && !isPathwayOpen ? { opacity: 0.5 } : {}
+            item.title === "PathWays" && !isPathwayOpen ? { opacity: 0.5 } : {}
           }>
           <AnimatePresence>
             {hoveredIndex === idx && (
